4ca5ec7389c43a8f5e791578496be6a73ad31c93
slipperyeel <jamesrpower+git@gmail.com>
2017-02-25 13:48:35 -0400

    Some minor changes to prefab management. Camera script
    Camera Script doesn't do anything right now, just lerps to the player.
    Just need to get something in there.

---
+++ Assets/Scripts/Camera/CameraController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace VldateSck
+{
+    public class CameraController : MonoBehaviour
+    {
+        [SerializeField]
+        private float mLerpFactor = 0.6f;
+
+        private PlayerCharacter mPlayerCharacter;
+
+        private float mCameraZ = 0.0f;
+
+        void Awake()
+        {
+            mCameraZ = transform.position.z;
+        }
+
+        public void Initialize(PlayerCharacter playerCharacter)
+        {
+            mPlayerCharacter = playerCharacter;
+        }
+
+        void Update()
+        {
+            // Something with mouse position and character position..
+            // Lerp this so it's smooth.
+            // Position between the screen bounds and the player? Consider it a box that lerps to the central point.
+            // for now
+            Vector3 curPos = transform.position;
+
+            Vector3 playerPos = mPlayerCharacter.transform.position;
+
+            Vector3 newPos = Vector3.Lerp(curPos, playerPos, mLerpFactor * Time.fixedDeltaTime);
+
+            newPos.z = mCameraZ;
+
+            transform.position = newPos;
+        }
+    }
+}
+
--- Assets/Scripts/Game/GameManager.cs
+++ Assets/Scripts/Game/GameManager.cs
@@ -2,14 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace TopDownShooter
+namespace VldateSck
 {
     public class GameManager : Singleton<GameManager>
     {
+        [SerializeField]
+        private CameraController mCameraController;
+
+        [SerializeField]
+        private PlayerCharacter mPlayerCharacter;
+
         // Use this for initialization
         void Start()
         {
-
+            mCameraController.Initialize(mPlayerCharacter);
         }
 
         // Update is called once per frame
